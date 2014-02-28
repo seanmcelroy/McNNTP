7b545fa0da7772f8baf44428a85a0a4ddad8665a
Sean McElroy <me@seanmcelroy.com>
2014-02-28 12:44:32 -0600

    Fixed a vexing bug caused on POST w/ Thunderbird
    When Thunderbird sends a short article, it may split the body's final
    delimiter "." into a separate transmission of just that with the final
    \r\n terminator.  When that occurred, a bug occurred where it was
    treated as a new message to Post, which would hang on the connection.
    Fixed.

--- McNNTP/Server/Connection.cs
+++ McNNTP/Server/Connection.cs
@@ -211,8 +211,11 @@ namespace McNNTP.Server
             if (_inProcessCommand != null && _inProcessCommand.MessageHandler != null)
             {
                 // Ongoing read - don't parse it for commands
-                var result = _inProcessCommand.MessageHandler.Invoke(content, _inProcessCommand);
-                if (result.IsQuitting)
+                _logger.InfoFormat("Content: \"{0}\"", content);
+                if (content == ".\r\n")
+                    _logger.InfoFormat("DEBUG");
+                _inProcessCommand = _inProcessCommand.MessageHandler.Invoke(content, _inProcessCommand);
+                if (_inProcessCommand.IsQuitting)
                     _inProcessCommand = null;
             }
             else
